46db62f93dc4df14eebb02497f5333bd2bc14935
Bronak <vovanicka@gmail.com>
2022-08-22 15:37:25 +0300

    Created basic movement

---
+++ StolenRails/Assets/_Solution/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            position.x += 1f*Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.z += 1f * Time.deltaTime;
+        }
+        transform.position = position;
+    }
+}
