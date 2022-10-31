f071c1097f5f2175c4763635ed53260ede4f7bc8
Bromak <vovanicka@gmail.com>
2022-10-31 19:12:49 +0200

    Update Sprint

--- StolenRails/Assets/_Solution/Scripts/Mover.cs
+++ StolenRails/Assets/_Solution/Scripts/Mover.cs
@@ -6,11 +6,32 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] private float _speed = 1;
     [SerializeField] private  CharacterController _characterController;
+    private bool isinsprint = false;
+    private bool issprintposible = true;
 
+    private IEnumerator sprint()
+
+    {
+        yield return new WaitForSeconds(0.2f);
+        isinsprint = false;
+        _speed = 1;
+        yield return new WaitForSeconds(3f);
+        issprintposible = true;
+
+    }
     void Update()
     {
         
         Vector3 motion = Vector3.zero;
+        if (issprintposible&&!isinsprint&&Input.GetKey(KeyCode.LeftShift))
+        {
+            issprintposible = false;
+            isinsprint = true;
+            StartCoroutine(sprint());
+            _speed = 3;
+            
+        }
+            
         if (Input.GetKey(KeyCode.D))
         {
             motion.x += _speed * Time.deltaTime;
